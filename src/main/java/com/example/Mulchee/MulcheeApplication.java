9665d7efda5fe19c4dc8e9da8c619c714f13a10a
Nanome Dev <dev@nanome.ai>
2023-05-01 09:30:35 -0400

    First commit

---
+++ src/main/java/com/example/Mulchee/MulcheeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mulchee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulcheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulcheeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mulchee/MulcheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mulchee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulcheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
